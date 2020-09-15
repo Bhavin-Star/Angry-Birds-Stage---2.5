288070a532afe24bf2ae6636cb33ba93a824b23e
Bhavin-Star <70592387+Bhavin-Star@users.noreply.github.com>
2020-09-15 20:53:45 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,15 @@
+class Bird extends base { 
+    constructor(x, y) {
+       
+      super(x,y,50,50);
+      this.image = loadImage('sprites/bird.png');
+      World.add(world, this.body);
+    }
+display(){
+  this.body.position.x = mouseX;
+  this.body.position.y = mouseY;
+  super.display();
+
+}
+   };
+  
